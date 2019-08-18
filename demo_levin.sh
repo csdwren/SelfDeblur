a53b97e231834c56eb1bf46286ee9ec4a2a96d55
csdwren <rendongweihit@gmail.com>
2019-08-18 19:18:26 +0800

    update

--- demo_levin.sh
+++ demo_levin.sh
@@ -7,5 +7,5 @@ python selfdeblur_levin_iter5k.py
 # 20000 iterations usually lead to better results than those in the paper. 
 python selfdeblur_levin_iter20k.py 
 
-# Given our learned deep models, we can directly reproduce the results reported in the paper. (still have small bugs, and will come soon)
+# Given our learned deep models, we can directly reproduce the results reported in the paper. 
 # python selfdeblur_levin_nonblind.py  
